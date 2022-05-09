d77a5c375953b64ac7c50f74de283f2fbb6b5d72
quintik <28828855+quintik@users.noreply.github.com>
2022-05-09 19:30:12 +0800

    move parser/ast stuff into its own directory

--- test/parser.ts
+++ test/parser.ts
@@ -1,6 +1,6 @@
-import { parse } from "../lib/parser.ts";
-import { scan } from "../lib/scanner.ts";
-import { Node, nodeToStr } from "../lib/ast.ts";
+import { parse } from "../lib/parser/parser.ts";
+import { scan } from "../lib/lexer/lexer.ts";
+import { Node, nodeToStr } from "../lib/parser/ast.ts";
 
 // NOTE: The spaces are important for now since our scanner
 // NOTE: still has some bugs that need to be fixed.
