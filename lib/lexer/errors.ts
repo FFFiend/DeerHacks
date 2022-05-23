661c442d478952b5f4443994876ea67c7bca0cc4
quintik <28828855+quintik@users.noreply.github.com>
2022-05-23 19:42:09 +0800

    feat(lexer): more user-friendly error printing

--- lib/lexer/errors.ts
+++ lib/lexer/errors.ts
@@ -12,7 +12,6 @@ export class UnrecognizedCharError extends Error {
     }
 
     public print(): void {
-        console.log("UNIMPLEMENTED!");
     }
 }
 
@@ -38,11 +37,12 @@ export class UnexpectedCharError extends Error {
 }
 
 export class UnclosedSequenceError extends Error {
+    private readonly expected: string;
     private readonly state: State;
+    private readonly hint: string;
 
-    public readonly expected: string;
+    public readonly description: string;
     public readonly fatal: boolean;
-    public readonly hint: string;
 
     public constructor(state: State, expected: string, hint: string) {
         super();
@@ -50,10 +50,70 @@ export class UnclosedSequenceError extends Error {
 
         this.expected = expected;
         this.fatal = false;
-        this.hint = hint;
+        this.hint = hint.split("\n").map(s => "Hint: " + s).join("\n")
+
+        const st = this.state;
+        this.message = `LexerError: UnclosedSequenceError at ${st.row}:${st.col}`;
+        this.description = `Expected a '${this.expected}' to close the sequence started at line ${st.row}, column ${st.col}, but found none.`;
     }
 
+    // TODO: Colors.
     public print(): void {
-        console.log("UNIMPLEMENTED!");
+        const st = this.state;
+        const lines = st.source.split("\n");
+        const gutters = getGutters(st);
+
+        // Print the message.
+        console.log(this.message);
+        console.log()
+
+        // Print the source line just before where the error occurred.
+        const prevLineSrc = st.row - 2 >= 0 ? lines[st.row - 2] : "";
+        console.log(gutters[0] + " | " + prevLineSrc);
+
+        // Print the error line.
+        const errLineSrc = lines[st.row - 1];
+        console.log(gutters[1] + " | " + errLineSrc);
+        // Print the column where error occurred and the error description.
+        const gutter = " ".repeat(gutters[1].length) + " | ";
+        const padding = " ".repeat(st.col - 1);
+        console.log(gutter + padding + "^ - " + this.description);
+
+        // Print the next line in source.
+        const nextLineSrc = st.row < lines.length ? lines[st.row] : "";
+        console.log(gutters[2] + " | " + nextLineSrc);
+        console.log();
+
+        // Print the hint.
+        console.log(this.hint);
     }
+}
+
+// Helpers
+function getGutters(st: State): string[] {
+    const lines = st.source.split("\n");
+    const gutterSize = 1 + (st.row + 1).toString().length;
+
+    const prevRow = st.row - 1;
+    const prevRowLength = prevRow.toString().length;
+    const prevLine
+        = prevRow < 1
+            ? " ".repeat(gutterSize)
+            : " ".repeat(gutterSize - prevRowLength) + prevRow.toString();
+
+    const nextRow = st.row + 1;
+    const nextRowLength = nextRow.toString().length;
+    const nextLine
+        = nextRow > lines.length
+            ? " ".repeat(gutterSize)
+            : " " + nextRow.toString();
+
+    const errRow = st.row;
+    const errRowLength = errRow.toString().length;
+    const errLine
+        = errRowLength < nextRowLength
+            ? "  " + errRow.toString()
+            : " " + errRow.toString();
+
+    return [prevLine, errLine, nextLine];
 }
